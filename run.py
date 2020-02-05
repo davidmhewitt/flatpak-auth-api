31afb17de384c305d52bd033903aa263d2733b40
David Hewitt <davidmhewitt@gmail.com>
2020-02-05 16:40:37 +0000

    Initial commit

---
+++ run.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+api = Api(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'some-secret-string'
+app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+
+db = SQLAlchemy(app)
+jwt = JWTManager(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+import views, models, resources
+
+api.add_resource(resources.UserRegistration, '/registration')
+api.add_resource(resources.UserLogin, '/login')
+api.add_resource(resources.TokenRefresh, '/token/refresh')
+api.add_resource(resources.SecretResource, '/secret')
